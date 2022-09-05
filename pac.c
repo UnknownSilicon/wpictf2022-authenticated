f372e1b8fcf490735f7095e01b566a7220cdbb90
UnknownSilicon <14339279+UnknownSilicon@users.noreply.github.com>
2022-09-05 16:39:10 -0400

    Basic working example

--- pac.c
+++ pac.c
@@ -6,9 +6,9 @@
 
 void sign(struct authenticate* auth, int context) {
     __asm__(
-        "LDR x7, [x1, #0xA]\r\n"
-        "PACGA x4, x7, x2\r\n"
-        "STR x4, [x7, #0x12]\r\n"
+        "LDR x9, [x0, #0xA]\r\n"
+        "PACGA x10, x9, x1\r\n"
+        "STR x10, [x0, #0x12]\r\n"
     );
 }
 
@@ -24,10 +24,10 @@ struct authenticate* initialize(int context) {
 
 int verify(struct authenticate* auth, int context) {
     __asm__(
-        "LDR x7, [x1, #0xA]\r\n"  // Load `isAdmin` into x7
-        "PACGA x4, x7, x2\r\n"    // Compute the PAC for x7 using `context`
-        "LDR x8, [x1, #0x12]\r\n" // Load the previous PAC into x8
-        "CMP x4, x8\r\n"          // Compare the two PACs
+        "LDR x9, [x0, #0xA]\r\n"  // Load `isAdmin` into x7
+        "PACGA x10, x9, x1\r\n"    // Compute the PAC for x7 using `context`
+        "LDR x11, [x0, #0x12]\r\n" // Load the previous PAC into x8
+        "CMP x10, x11\r\n"          // Compare the two PACs
         "BEQ end\r\n"             // If they are equal, jump
         "MOV x9, #0x0\r\n"        // Jump to 0x0
         "BR x9\r\n"
@@ -40,7 +40,7 @@ int verify(struct authenticate* auth, int context) {
 int main(int argc, char* argv[]) {
     struct authenticate* auth = initialize(123);
 
-    
+    //verify(auth, 123);
 
     return 0;
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 
-qemu-aarch64 -cpu max,pauth=off -L /usr/aarch64-linux-gnu ./pac.elf
+qemu-aarch64 -cpu max,pauth=on -L /usr/aarch64-linux-gnu ./pac.elf
